a8dbebd3200af93990befc6ee7c43e8b65c65e5b
Gakkk23 <navy02495@gmail.com>
2022-03-17 21:52:59 +0900

    テストコードの実装(新規登録できる場合)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,13 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it 'すべての項目を正しく入力すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+
     context '新規登録できない場合' do
       it 'emailが空では登録できない' do
         @user.email = ''
