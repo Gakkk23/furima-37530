2d0da57bd41debd3343285dbe0ef3d179f757aba
Gakkk23 <navy02495@gmail.com>
2022-04-20 10:51:37 +0900

    ActiveStorageの導入

--- Gemfile
+++ Gemfile
@@ -64,3 +64,7 @@ gem 'active_hash'
 gem 'devise'
 
 gem 'pry-rails'
+
+gem 'mini_magick'
+
+gem 'image_processing', '~> 1.2'
--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,2 +1,8 @@
 class MarketsController < ApplicationController
+
+  private
+
+  def market_params
+      params.require(:market).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
