aba54fb5f97c57b72cafa41ae07959274ea99312
Gakkk23 <navy02495@gmail.com>
2022-04-28 16:37:51 +0900

    バリデーションの設定

--- app/models/market.rb
+++ app/models/market.rb
@@ -12,6 +12,23 @@ class Market < ApplicationRecord
   has_one_attached :image
 
   
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :postage_payer_id
+    validates :prefecture_id
+    validates :shipping_date_id
+    validates :price
+    validates :image
+    validates :user_id
+  end
+
+  with_options numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 } do
+    validates :price
+  end
+  
   with_options numericality: { other_than: 0 } do
     validates :category_id
     validates :condition_id
