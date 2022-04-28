eb38e39fb0009d96ded9ad2bb3320139ec954f93
Gakkk23 <navy02495@gmail.com>
2022-04-28 16:36:16 +0900

    ActiveHashの導入

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,3 +1,23 @@
 class Market < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  #ActiveHash
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage_payer
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  #ActiveStorage
   has_one_attached :image
+
+  
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :condition_id
+    validates :postage_payer_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
+
 end
